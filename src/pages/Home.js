3729171844c58e3f39b1d093097b7b460a1fd530
jessyv96 <c.j91350@gmail.com>
2020-12-23 18:38:01 +0100

    Can display the photographer, hovering photo #2

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,12 +3,16 @@ import m from "mithril";
 function Home() {
     let photos;
     const displayAuthorHandler = (e, id) => {
+        const photo = document.getElementById(`photo${id}`)
         const author = document.getElementById(`author${id}`)
         if (e.type === 'mouseenter') {
+            photo.style.opacity = 0.7
             author.style.opacity = 1
         }
         if (e.type === 'mouseleave') {
-            author.style.opacity = 0        }
+            author.style.opacity = 0
+            photo.style.opacity = 1
+        }
     }
 
     return {
@@ -24,18 +28,18 @@ function Home() {
             <main>
                 <div class='gallery'>
                     {photos && photos.map((photo, index) => (
-                        <div key={photo.id} class={`photo-${index}`}>
+                        <div
+                            onmouseenter={(e) => displayAuthorHandler(e, photo.id)}
+                            onmouseleave={(e) => displayAuthorHandler(e, photo.id)}
+                            key={photo.id}
+                            class={`photo-${index}`}>
                             <img
                                 id={`photo${photo.id}`}
-                                onmouseenter={(e) => displayAuthorHandler(e, photo.id)}
-                                onmouseleave={(e) => displayAuthorHandler(e, photo.id)}
                                 title='mon image'
                                 src={`${photo.download_url}`}
                                 alt='nom_image' />
-                            <div 
-                                onmouseenter={(e) => displayAuthorHandler(e, photo.id)}
-                                onmouseleave={(e) => displayAuthorHandler(e, photo.id)}
-                                id={`author${photo.id}`} 
+                            <div
+                                id={`author${photo.id}`}
                                 class='author-container'>
                                 <h2 class='author-name'>{photo.author}</h2>
                             </div>
