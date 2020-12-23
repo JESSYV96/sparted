fb3ccc3a6514d9a6a9ca4d3339db4c152397684d
jessyv96 <c.j91350@gmail.com>
2020-12-23 19:50:48 +0100

    Create photo component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,8 @@
 import m from "mithril";
+import Photo from '../components/Photo.js'
 
 function Home() {
     let photos;
-    const displayAuthorHandler = (e, id) => {
-        const photo = document.getElementById(`photo${id}`)
-        const author = document.getElementById(`author${id}`)
-        if (e.type === 'mouseenter') {
-            photo.style.opacity = 0.7
-            author.style.opacity = 1
-        }
-        if (e.type === 'mouseleave') {
-            author.style.opacity = 0
-            photo.style.opacity = 1
-        }
-    }
 
     return {
         oncreate: () => {
@@ -27,24 +16,12 @@ function Home() {
         view: () => (
             <main>
                 <div class='gallery'>
-                    {photos && photos.map((photo, index) => (
-                        <div
-                            onmouseenter={(e) => displayAuthorHandler(e, photo.id)}
-                            onmouseleave={(e) => displayAuthorHandler(e, photo.id)}
-                            key={photo.id}
-                            class={`photo-${index}`}>
-                            <img
-                                id={`photo${photo.id}`}
-                                title='mon image'
-                                src={`${photo.download_url}`}
-                                alt='nom_image' />
-                            <div
-                                id={`author${photo.id}`}
-                                class='author-container'>
-                                <h2 class='author-name'>{photo.author}</h2>
-                            </div>
-                        </div>
-                    ))}
+                    {photos && photos.map((photo, index) => <Photo
+                        index={index}
+                        id={photo.id}
+                        author={photo.author}
+                        imageUrl={photo.download_url} />
+                    )}
                 </div>
             </main>
         )
