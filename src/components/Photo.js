0a99559845b04c684afbc5cb470e591bda96d90c
jessyv96 <c.j91350@gmail.com>
2020-12-25 11:49:37 +0100

    Modal real size photo

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -13,23 +13,27 @@ function Photo() {
             photo.style.opacity = 1
         }
     }
+
     return {
         view: (ctrl) => (
-            <div
-                onmouseenter={(e) => displayAuthorHandler(e, ctrl.attrs.id)}
-                onmouseleave={(e) => displayAuthorHandler(e, ctrl.attrs.id)}
-                class={`photo-${ctrl.attrs.index}`}>
-                <img
-                    id={`photo${ctrl.attrs.id}`}
-                    title='mon image'
-                    src={`${ctrl.attrs.imageUrl}`}
-                    alt='nom_image' />
+            <>
                 <div
-                    id={`author${ctrl.attrs.id}`}
-                    class='author-container'>
-                    <h2 class='author-name'>{ctrl.attrs.author}</h2>
+                    onclick={ctrl.attrs.displayModal}
+                    onmouseenter={(e) => displayAuthorHandler(e, ctrl.attrs.id)}
+                    onmouseleave={(e) => displayAuthorHandler(e, ctrl.attrs.id)}
+                    class={`photo-${ctrl.attrs.index}`}>
+                    <img
+                        id={`photo${ctrl.attrs.id}`}
+                        title='mon image'
+                        src={`${ctrl.attrs.imageUrl}`}
+                        alt='nom_image' />
+                    <div
+                        id={`author${ctrl.attrs.id}`}
+                        class='author-container'>
+                        <h2 class='author-name'>{ctrl.attrs.author}</h2>
+                    </div>
                 </div>
-            </div>
+            </>
         )
     }
 }
